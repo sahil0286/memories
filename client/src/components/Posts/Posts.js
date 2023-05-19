ee84d80b2d7fd17c9fec490c3d5a97c05ff0ff0c
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-05-19 23:59:26 +0530

    Update Posts.js

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { Post } from './Post/Post'
-
 export const Posts = () => {
   const posts = useSelector((state)=> state.posts);
   console.log(posts)
