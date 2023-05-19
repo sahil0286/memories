f98d7f7bc2a10f21546bdce5d6e1af1f80117ea4
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-05-20 00:00:01 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,6 @@ const app = express();
 app.use(bodyParser.json({ limit: "30mb" , extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb" , extended: true}));
 app.use(cors());
-
 app.use("/posts",postRoutes);
 const CONNECTION_URL = "mongodb+srv://sahil:sahil123@cluster0.xqlqfsz.mongodb.net/?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
