15ae7865b37ebcc57562794d0ee55b37d286e3e3
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-05-19 21:53:33 +0530

    Update Navbar.js

--- client/src/subcomponents/Navbar.js
+++ client/src/subcomponents/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Navbar = () => {
   return (
     <>
-        <header className="p-3 text-bg-dark">
+    <header className="p-3 text-bg-dark">
     <div className="container">
       <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
         <a href="/" className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
