2777de066892b3ac574d0d9e2bc783f624c311cd
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-05-19 22:53:53 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,13 @@
 import React from "react";
 import { createRoot }  from "react-dom/client";
+import { Provider } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk"
+
+import reducers from "./reducers";
 
 import App from "./App";
+
+const store= createStore(reducers, compose(applyMiddleware(thunk)))
 
 createRoot(document.getElementById("root")).render(<App/>);
