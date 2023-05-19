237c4d41d7ce4ec475fbae95ecd22c8eb291b0c5
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-05-19 21:53:20 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { Navbar } from './subcomponents/Navbar';
+import { Posts } from './components/Posts/Posts';
+import { Forms } from './components/Form/Forms';
 
 
 export default function App() {
   return (
     <>
     <Navbar/>
+    <div class="row">
+      <div class="col-7 border">
+        <Posts/>
+      </div>
+      <div class="col-5 border">
+        <Forms/>
+      </div>
+    </div>
     </>
   )
 }
